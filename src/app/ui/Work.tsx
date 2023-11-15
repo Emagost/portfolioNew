cbe290bc824ef5cea7b55068983672573308790f
Ema Gomez <emagozz@gmail.com>
2023-11-15 19:31:41 -0300

    Add WorkPage

---
+++ src/app/ui/Work.tsx
@@ -0,0 +1,14 @@
+export default function WorkPage() {  
+  return (
+    <section>
+      <div className="flex items-center h-screen overflow-x-hidden bg-gradient-to-b from-[#1A1840] from-5% to-black to-15%">
+        <h2 className="text-4xl transform -rotate-90 origin-left text-white px-4 py-2 ml-6">
+          Work
+        </h2>
+        <div className="flex-grow">
+          {/* Contenido de tu página aquí */}
+        </div>
+      </div>
+    </section>
+  )
+}
